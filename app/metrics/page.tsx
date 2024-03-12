b8f0535635d35b303f65171b400fcd7f4a281f46
Alvin Quach <alvinwquach@gmail.com>
2024-03-11 19:29:07 -0700

    Add tooltip to skills frequency chart

--- app/metrics/page.tsx
+++ app/metrics/page.tsx
@@ -4,7 +4,6 @@ import { useEffect, useState } from "react";
 import { getUserJobSkillsAndFrequency } from "../lib/getUserJobSkillsAndFrequency";
 import Chart from "chart.js/auto";
 
-
 function Metrics(): JSX.Element {
   const [sortedSkills, setSortedSkills] = useState<string[]>([]);
   const [sortedFrequencies, setSortedFrequencies] = useState<number[]>([]);
@@ -27,67 +26,73 @@ function Metrics(): JSX.Element {
 
   useEffect(() => {
     if (sortedSkills.length > 0) {
-      // Draw chart when sortedSkills or statusPercentages change
+      // Draw chart when sortedSkills or sortedFrequencies change
       const chartContainer = document.getElementById("skillFrequencyChart");
-      if (
-        chartContainer &&
-        Chart.getChart(chartContainer as HTMLCanvasElement)
-      ) {
-        Chart.getChart(chartContainer as HTMLCanvasElement)?.destroy();
-      }
-      new Chart(chartContainer as HTMLCanvasElement, {
-        type: "bar",
-        data: {
-          labels: sortedSkills,
-          datasets: [
-            {
-              label: "Skill Frequency",
-              data: sortedFrequencies,
-              backgroundColor: "steelblue",
-            },
-          ],
-        },
-        options: {
-          indexAxis: "y",
-          responsive: true,
-          maintainAspectRatio: false,
-          scales: {
-            x: {
-              title: {
-                display: true,
-                text: "Frequency",
-                color: "#fff",
+      if (chartContainer) {
+        const chart = new Chart(chartContainer as HTMLCanvasElement, {
+          type: "bar",
+          data: {
+            labels: sortedSkills,
+            datasets: [
+              {
+                label: "Skill Frequency",
+                data: sortedFrequencies,
+                backgroundColor: "steelblue",
               },
-              ticks: {
-                color: "#fff",
-                font: {
-                  size: 10,
+            ],
+          },
+          options: {
+            indexAxis: "y",
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+              x: {
+                title: {
+                  display: true,
+                  text: "Frequency",
+                  color: "#fff",
+                },
+                ticks: {
+                  color: "#fff",
+                  font: {
+                    size: 10,
+                  },
+                },
+                beginAtZero: true,
+              },
+              y: {
+                title: {
+                  display: true,
+                  text: "Skills",
+                  color: "#fff",
+                },
+                ticks: {
+                  color: "#fff",
                 },
               },
-              beginAtZero: true,
             },
-            y: {
-              title: {
-                display: true,
-                text: "Skills",
-                color: "#fff",
+            plugins: {
+              tooltip: {
+                enabled: true,
+                callbacks: {
+                  label: (tooltipItem) => {
+                    const dataset = tooltipItem.chart.data.datasets[0];
+                    const value = dataset.data[tooltipItem.dataIndex];
+                    return `${tooltipItem.label}: ${value}`;
+                  },
+                },
               },
-              ticks: {
-                color: "#fff",
+              legend: {
+                display: false,
               },
             },
+            onClick: () => {},
           },
-          plugins: {
-            legend: {
-              display: false,
-            },
-            tooltip: {
-              enabled: false,
-            },
-          },
-          onClick: () => {},
-        },
-      });
+        });
+        return () => {
+          chart.destroy();
+        };
+      }
     }
   }, [sortedSkills, sortedFrequencies]);
 
