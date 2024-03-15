8e38554e0a6f653c6569eb93967c968f5bbbc0b3
Alvin Quach <alvinwquach@gmail.com>
2024-03-14 22:55:20 -0700

    Fix show more and show less on suggested skills card

--- app/components/profile/SuggestedSkillsCard.tsx
+++ app/components/profile/SuggestedSkillsCard.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React, { useState } from "react";
 
 interface SuggestedSkillsCardProps {
@@ -15,18 +13,21 @@ function SuggestedSkillsCard({
   const [visibleSkills, setVisibleSkills] = useState(minSkillsToShow);
 
   const userSkillsArray = Array.isArray(userSkills) ? userSkills : [];
-
   const missingSkills = suggestedSkills.filter(
     (skill) => !userSkillsArray.includes(skill)
   );
 
+  const showMoreSkills = visibleSkills < missingSkills.length;
+  const showLessSkills = visibleSkills > minSkillsToShow;
+
   const handleShowMore = () => {
-    setVisibleSkills((prev) => Math.min(prev + 5, missingSkills.length));
+    setVisibleSkills((prev) => Math.max(minSkillsToShow, prev + 5));
   };
 
   const handleShowLess = () => {
     setVisibleSkills((prev) => Math.max(minSkillsToShow, prev - 5));
   };
+
   return (
     <div className="bg-gray-800 p-8 rounded-lg shadow-lg max-w-4xl mx-auto">
       <h2 className="text-xl font-bold text-gray-200 mb-4">Suggested Skills</h2>
@@ -38,30 +39,32 @@ function SuggestedSkillsCard({
         {missingSkills.slice(0, visibleSkills).map((missingSkill, index) => (
           <span
             key={index}
-            className="bg-gray-600 text-white rounded-lg px-3 py-1 text-sm font-semibold"
+            className="bg-gray-600 text-white rounded-lg px-3 py-1 text-base "
           >
             {missingSkill}
           </span>
         ))}
       </div>
-      {visibleSkills < missingSkills.length ? (
+      {(showMoreSkills || showLessSkills) && (
         <div className="flex justify-center mt-4">
-          <button
-            onClick={handleShowMore}
-            className="text-gray-300 hover:text-white text-sm font-semibold mr-4"
-          >
-            Show more
-          </button>
-          {visibleSkills > minSkillsToShow && (
+          {showMoreSkills && (
+            <button
+              onClick={handleShowMore}
+              className="text-gray-400 mt-2 text-sm hover:text-gray-200 focus:outline-none relative z-10"
+            >
+              Show more
+            </button>
+          )}
+          {showLessSkills && (
             <button
               onClick={handleShowLess}
-              className="text-gray-300 hover:text-white text-sm font-semibold"
+              className="text-gray-400 mt-2 ml-2 text-sm hover:text-gray-200 focus:outline-none relative z-10"
             >
               Show less
             </button>
           )}
         </div>
-      ) : null}
+      )}
     </div>
   );
 }
