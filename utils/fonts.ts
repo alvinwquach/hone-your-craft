20cbc1f21945cc4ebb6924b6889c00c7051940e6
Alvin Quach <alvinwquach@gmail.com>
2024-03-31 15:51:22 -0700

    Fix

--- utils/fonts.ts
+++ utils/fonts.ts
@@ -3,6 +3,5 @@ import { Rubik_Scribble } from "next/font/google";
 export const rubik_scribble = Rubik_Scribble({
   weight: ["400"],
   style: ["normal"],
-  subsets: ["latin"],
   display: "swap",
 });
