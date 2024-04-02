1b1ae0a6d324c9e79ac31d3e7b2992a8a4929cae
Alvin Quach <alvinwquach@gmail.com>
2024-04-02 16:25:23 -0700

    Truncate title and company on interview calendar

--- app/components/calendar/InterviewCalendar.tsx
+++ app/components/calendar/InterviewCalendar.tsx
@@ -42,6 +42,87 @@ const InterviewDay = ({ interview }: { interview: any }) => {
   const [showOptionsMenu, setShowOptionsMenu] = useState(false);
   const optionsMenuRef = useRef<HTMLDivElement>(null);
 
+  const truncateTitle = (title: string, maxLength: number) => {
+    let truncatedTitle = title;
+
+    const screenWidth = window.screen.width;
+    const screenHeight = window.screen.height;
+
+    switch (true) {
+      case screenWidth === 1024 && screenHeight === 1366: // iPad Pro
+        truncatedTitle = title.length > 11 ? `${title.slice(0, 8)}...` : title;
+        break;
+      case screenWidth === 768 && screenHeight === 1024: // iPad Mini
+        truncatedTitle = title.length > 17 ? `${title.slice(0, 16)}...` : title;
+        break;
+      case screenWidth === 820 && screenHeight === 1180: // iPad Air
+        truncatedTitle = title.length > 16 ? `${title.slice(0, 15)}...` : title;
+        break;
+      case screenWidth === 912 && screenHeight === 1368: // Surface Pro 7
+        truncatedTitle = title.length > 21 ? `${title.slice(0, 20)}...` : title;
+        break;
+      case screenWidth === 853 && screenHeight === 1280: // Asus Zenbook Fold
+        truncatedTitle = title.length > 18 ? `${title.slice(0, 17)}...` : title;
+        break;
+      case screenWidth === 1024 && screenHeight === 600: // Nest Hub
+        truncatedTitle = title.length > 10 ? `${title.slice(0, 9)}...` : title;
+        break;
+      case screenWidth === 1280 && screenHeight === 800: // Nest Hub Max
+        truncatedTitle = title.length > 14 ? `${title.slice(0, 13)}...` : title;
+        break;
+      default:
+        truncatedTitle =
+          title.length > maxLength ? `${title.slice(0, maxLength)}...` : title;
+    }
+
+    return truncatedTitle;
+  };
+
+  const truncateCompany = (company: string, maxLength: number) => {
+    let truncatedCompany = company;
+
+    const screenWidth = window.screen.width;
+    const screenHeight = window.screen.height;
+
+    switch (true) {
+      case screenWidth === 1024 && screenHeight === 1366: // iPad Pro
+        truncatedCompany =
+          company.length > 11 ? `${company.slice(0, 10)}...` : company;
+        break;
+      case screenWidth === 768 && screenHeight === 1024: // iPad Mini
+        truncatedCompany =
+          company.length > 17 ? `${company.slice(0, 16)}...` : company;
+        break;
+      case screenWidth === 820 && screenHeight === 1180: // iPad Air
+        truncatedCompany =
+          company.length > 18 ? `${company.slice(0, 17)}...` : company;
+        break;
+      case screenWidth === 912 && screenHeight === 1368: // Surface Pro 7
+        truncatedCompany =
+          company.length > 20 ? `${company.slice(0, 19)}...` : company;
+        break;
+      case screenWidth === 853 && screenHeight === 1280: // Asus Zenbook Fold
+        truncatedCompany =
+          company.length > 18 ? `${company.slice(0, 17)}...` : company;
+        break;
+      case screenWidth === 1024 && screenHeight === 600: // Nest Hub
+        truncatedCompany =
+          company.length > 11 ? `${company.slice(0, 10)}...` : company;
+        break;
+      case screenWidth === 1280 && screenHeight === 800: // Nest Hub Max
+        truncatedCompany =
+          company.length > 16 ? `${company.slice(0, 15)}...` : company;
+        break;
+      default:
+        truncatedCompany =
+          company.length > maxLength
+            ? `${company.slice(0, maxLength)}...`
+            : company;
+    }
+
+    return truncatedCompany;
+  };
+
   const closeModal = () => {
     setIsModalOpen(false);
   };
@@ -82,11 +163,13 @@ const InterviewDay = ({ interview }: { interview: any }) => {
     <div
       className={`flex flex-col ${getColorForInterviewType(
         interviewType
-      )} bg-opacity-80 rounded-md p-2 text-sm relative`}
+      )} bg-opacity-80 rounded-md p-2 gap-1 text-sm relative`}
     >
-      <div className="text-xs font-semibold mt-1">{title}</div>
-      <div className="text-sm mt-1">{company}</div>
-      <div className="text-xs mt-1">
+      <div className="text-xs font-semibold whitespace-nowrap">
+        {truncateTitle(title, 40)}
+      </div>
+      <div className="text-xs ">{truncateCompany(company, 40)}</div>
+      <div className="text-xs ">
         <div>{format(new Date(date), "h:mm a")}</div>
       </div>
       <div className="absolute top-0 right-0">
