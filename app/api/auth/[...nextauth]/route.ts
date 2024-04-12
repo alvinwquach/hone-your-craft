5aad05a8220da88990f8731a2ae3b3d2e728cf18
Alvin Quach <alvinwquach@gmail.com>
2024-04-12 11:44:39 -0700

    Add max duration

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 import authOptions from "./options";
 import NextAuth from "next-auth";
 export const dynamic = "force-dynamic"; 
+export const maxDuration = 20;
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
