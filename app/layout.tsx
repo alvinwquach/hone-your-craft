6c670cea7d4ffbdf2bcb5328a170a0b86c8ea69e
Alvin Quach <alvinwquach@gmail.com>
2024-11-03 15:30:24 -0800

    Add bottom navigation dock

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         <SessionProvider>
           <Header />
           <main className="">{children}</main>
-          {/* <BottomNavigationDock /> */}
+          <BottomNavigationDock />
           <ToastContainer />
         </SessionProvider>
       </body>
