c60b8b5893b8bb4de88d57dd5edfb67eca52b2be
Alvin Quach <alvinwquach@gmail.com>
2024-11-14 23:44:15 -0800

    Create post job form for client page

---
+++ app/post-job/page.tsx
@@ -0,0 +1,11 @@
+import PostJobForm from "../components/jobs/PostJobForm";
+
+function JobPosting() {
+  return (
+    <section className="max-w-screen-2xl mx-auto px-5 sm:px-6 lg:px-8 py-20 sm:py-24 lg:py-24 min-h-screen">
+      <PostJobForm />
+    </section>
+  );
+}
+
+export default JobPosting;
