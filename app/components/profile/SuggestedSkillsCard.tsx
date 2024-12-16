ffc53e7945848181e23ddcc24c0893e1639bc1f9
Alvin Quach <alvinwquach@gmail.com>
2024-12-16 15:27:35 -0800

    Sort suggested skills and create scrollable container

--- app/components/profile/SuggestedSkillsCard.tsx
+++ app/components/profile/SuggestedSkillsCard.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 interface SuggestedSkillsCardProps {
   suggestedSkills: string[];
   userSkills?: string[];
@@ -9,28 +7,15 @@ function SuggestedSkillsCard({
   suggestedSkills,
   userSkills = [],
 }: SuggestedSkillsCardProps) {
-  const minSkillsToShow = 5;
-  const [visibleSkills, setVisibleSkills] = useState(minSkillsToShow);
+  const userSkillsArray = userSkills || [];
 
-  const userSkillsArray = Array.isArray(userSkills) ? userSkills : [];
-  const missingSkills = suggestedSkills.filter(
-    (skill) => !userSkillsArray.includes(skill)
-  );
-
-  const showMoreSkills = visibleSkills < missingSkills.length;
-  const showLessSkills = visibleSkills > minSkillsToShow;
-
-  const handleShowMore = () => {
-    setVisibleSkills((prev) => Math.max(minSkillsToShow, prev + 5));
-  };
-
-  const handleShowLess = () => {
-    setVisibleSkills((prev) => Math.max(minSkillsToShow, prev - 5));
-  };
+  const missingSkills = suggestedSkills
+    .filter((skill) => !userSkillsArray.includes(skill))
+    .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
 
   return (
-    <div className="bg-gray-800 p-8 rounded-lg shadow-lg w-full max-w-lg mx-auto">
-      <h2 className="text-xl font-bold text-gray-200 mb-4">Suggested Skills</h2>
+    <div className="bg-zinc-900 p-6 rounded-lg shadow-lg w-full max-w-lg mx-auto border border-gray-700">
+      <h2 className="text-xl font-bold text-gray-100 mb-4">Suggested Skills</h2>
       {missingSkills.length === 0 && (
         <p className="text-gray-300 mb-4">
           Welcome! As you begin to apply to roles, we&apos;ll suggest skills to
@@ -43,36 +28,16 @@ function SuggestedSkillsCard({
           suggest adding to your profile:
         </p>
       )}
-      <div className="flex flex-wrap gap-2">
-        {missingSkills.slice(0, visibleSkills).map((missingSkill, index) => (
+      <div className="flex flex-wrap gap-2 max-h-60 overflow-y-auto pr-4">
+        {missingSkills.map((missingSkill, index) => (
           <span
             key={index}
-            className="bg-gray-600 text-white rounded-lg px-3 py-1 text-base "
+            className="bg-zinc-700 text-white rounded-lg px-3 py-1 text-base"
           >
             {missingSkill}
           </span>
         ))}
       </div>
-      {(showMoreSkills || showLessSkills) && (
-        <div className="flex justify-center mt-4">
-          {showMoreSkills && (
-            <button
-              onClick={handleShowMore}
-              className="text-gray-400 mt-2 text-sm hover:text-gray-200 focus:outline-none relative"
-            >
-              Show more
-            </button>
-          )}
-          {showLessSkills && (
-            <button
-              onClick={handleShowLess}
-              className="text-gray-400 mt-2 ml-2 text-sm hover:text-gray-200 focus:outline-none relative"
-            >
-              Show less
-            </button>
-          )}
-        </div>
-      )}
     </div>
   );
 }
