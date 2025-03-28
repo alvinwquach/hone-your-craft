00e9f0796c187fd21685f1876113844db946486c
Alvin Quach <alvinwquach@gmail.com>
2025-03-28 13:20:52 -0700

    Optimize query to track jobs

--- app/api/tracked-jobs/route.ts
+++ app/api/tracked-jobs/route.ts
@@ -7,21 +7,45 @@ import { unstable_cache } from "next/cache";
 const getCachedTrackedJobs = unstable_cache(
   async () => {
     console.time("getCachedTrackedJobs");
-
     const currentUser = await getCurrentUser();
     if (!currentUser) throw new Error("Unauthorized");
 
     console.time("prismaQuery");
-
     const jobs = await prisma.job.findMany({
       where: { userId: currentUser.id },
-      orderBy: [{ createdAt: "desc" }],
       select: {
-        company: true,
+        id: true,
         title: true,
+        company: true,
         postUrl: true,
         status: true,
+        createdAt: true,
+        description: true,
+        workLocation: true,
+        salary: true,
+        industry: true,
+        location: true,
+        interviews: {
+          select: {
+            interviewDate: true,
+            interviewType: true,
+          },
+        },
+        offer: {
+          select: {
+            salary: true,
+            offerDate: true,
+            offerDeadline: true,
+          },
+        },
+        rejection: {
+          select: {
+            initiatedBy: true,
+            notes: true,
+          },
+        },
       },
+      orderBy: [{ createdAt: "desc" }],
     });
     console.timeLog("prismaQuery");
 
@@ -37,18 +61,14 @@ const getCachedTrackedJobs = unstable_cache(
       "REJECTED",
     ];
 
-    const columnPromises = columnTypes.map((columnType) => {
-      const filteredJobs = jobs.filter((job) => job.status === columnType);
-      return {
-        id: columnType,
-        jobs: filteredJobs,
-      };
-    });
+    const columnPromises = columnTypes.map((columnType) => ({
+      id: columnType,
+      jobs: jobs.filter((job) => job.status === columnType),
+    }));
 
     const columnValues = await Promise.all(columnPromises);
     columnValues.forEach((column) => columns.set(column.id, column));
     console.timeLog("getCachedTrackedJobs");
-
     return Array.from(columns.values());
   },
   ["jobs"],
