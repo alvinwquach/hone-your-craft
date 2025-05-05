b813125f53c653b5469b6d65621ccf2ada7dd092
Alvin Quach <alvinwquach@gmail.com>
2025-05-05 12:20:36 -0700

    Update styling on meetings page

--- app/profile/meetings/page.tsx
+++ app/profile/meetings/page.tsx
@@ -31,8 +31,8 @@ export default async function Meetings() {
 
   return (
     <section className="flex-1 ml-16 md:ml-16 max-w-screen-2xl mx-auto px-5 sm:px-6 lg:px-8 py-20 sm:py-24 lg:py-24 min-h-screen">
-      <div className="container mx-auto p-4">
-        <div className="flex justify-between items-center mb-6">
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
           <h1 className="text-2xl font-bold text-gray-900 sr-only">
             Upcoming Meetings
           </h1>
@@ -42,12 +42,10 @@ export default async function Meetings() {
             <MeetingCalendarDownloadButton />
           )}
         </div>
-        <div className="pt-16">
-          <MeetingTabs
-            groupedUpcomingEvents={groupedUpcomingEvents}
-            groupedPastEvents={groupedPastEvents}
-          />
-        </div>
+        <MeetingTabs
+          groupedUpcomingEvents={groupedUpcomingEvents}
+          groupedPastEvents={groupedPastEvents}
+        />
       </div>
     </section>
   );
