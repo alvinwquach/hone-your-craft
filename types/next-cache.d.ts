e6b3a7851d29b35b8f30c470205d8093321d6c99
Alvin Quach <alvinwquach@gmail.com>
2025-07-10 15:31:04 -0700

    Fix

--- types/next-cache.d.ts
+++ types/next-cache.d.ts
@@ -1,4 +1,13 @@
 declare module "next/cache" {
   export function revalidateTag(tag: string): void;
   export function revalidatePath(path: string, type?: "page" | "layout"): void;
+
+  export function unstable_cache<T extends (...args: any[]) => any>(
+    fn: T,
+    keyParts?: string[],
+    options?: {
+      revalidate?: number | false;
+      tags?: string[];
+    }
+  ): T;
 }
